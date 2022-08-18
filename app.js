c71089bb9f037366e6da73bab2e36cca9fabb282
awoods1893 <awoods1983@gmail.com>
2022-08-18 09:55:07 -0500

    added the inquirer prompt and switch cases for all of the answers

--- app.js
+++ app.js
@@ -19,3 +19,53 @@ sqlConnection.connect(function(err){
     options();
 })
 
+function options() {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'Welcome to the employee database! Please choose an option.',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+            'Add an employee',
+            'Add a department',
+            'Add a role',
+            "Update employee role",
+            'Delete an employee',
+            'Exit',
+        ]
+    }).then (function (answer){
+        switch (answer.action){
+            case 'View all employees':
+                employeeView();
+                break;
+            case 'View all departments':
+                departmentView();
+                break;
+            case 'View all roles':
+                rolesView();
+                break;
+            case 'Add an employee':
+                employeeAdd();
+                break;
+            case 'Add a department':
+                departmentAdd();
+                break;
+            case 'Add a role':
+                roleAdd();
+                break;
+            case 'Update employee role':
+                roleUpdate();
+                    break;
+            case 'Delete an employee':
+                employeeDelete();
+                    break;
+            case 'EXIT': 
+                exitApp();
+                    break;
+            default:
+                    break;
+        }
+    })
+}
