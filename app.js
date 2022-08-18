e4df5e3ed0b58f5030148f175a5ec4ccbf24772e
awoods1893 <awoods1983@gmail.com>
2022-08-18 09:35:18 -0500

    added an encrypted env file to store the database credentials

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const mySQL = require('mysql');
 const inquirer = require('inquirer');
-const consoleTable = require('console.table')
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPassword = "SA@!$SDdfgFU%564$FSCVS"
-
-//async hashing and encryption function for password security
-bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(myPassword, salt, function(err, hash) {
-    });
-});
+const consoleTable = require('console.table');
+const secureEnv = require('secure-env');
+global.env = secureEnv({secret:'mySecretPassword'});
 
 //creating the connection to the mysql database
-const sqlConnection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'myuser',
-    password: myPassword,
+const sqlConnection = mySQL.createConnection({
+    host: global.env.DB_HOST,
+    port: global.env.DB_PORT,
+    user: global.env.DB_USER,
+    password: global.env.DB_PASS,
     database: 'employees_db',
 })
 
